4e52399b39ed596e0d764db733225663011ff7f2
Tussis88 <andreab88@hotmail.it>
2024-04-16 00:30:16 +0200

    aggiunte funzioni base

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("prova");
+// main operations
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// main variables
+let firstNumber;
+let secondNumber;
+let operator;
+
+// main function
+function operate(a, b, operation) {
+  return operation(a, b);
+}
+
+// HTML elements
+const calculator = document.querySelector("#calculator");
+
+const calcScreen =  document.createElement("div");
+calcScreen.id = "screen";
+calcScreen.textContent = "prova";
+
+const buttons = [];
+for (let i = 1; i < 10; i++) {
+  buttons[i] = document.createElement("button");
+  buttons[i].id = i;
+  buttons[i].textContent = i;
+  buttons[i].classList.add("numbers");
+  calculator.appendChild(buttons[i]);
+};
+
+calculator.appendChild(calcScreen);
