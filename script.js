45ac5b6c49eebb3f2cd80d08560988c2c88956c1
Tussis88 <andreab88@hotmail.it>
2024-04-18 00:42:01 +0200

    fatta logica addizzione
    fatto tasto = e CLEAR
    probabilmente si può incapsulare tutto quel codice all'interno di una
    funzione. Va bene usare tutte quelle variabili globali?

--- script.js
+++ script.js
@@ -16,21 +16,27 @@ function divide(a, b) {
 }
 
 // main variables
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
+let result = 0;
 let operator;
+let itsFirst = true;
 
 // main function
 function operate(a, b, operation) {
   return operation(a, b);
 }
 
+function calcLogic(number) {
+  return itsFirst ? (firstNumber += number) : (secondNumber += number);
+}
+
 // HTML elements
 const calculator = document.querySelector("#calculator");
 
-const calcScreen =  document.createElement("div");
+const calcScreen = document.createElement("div");
 calcScreen.id = "screen";
-calcScreen.textContent = "prova";
+calcScreen.textContent = "0";
 calculator.appendChild(calcScreen);
 
 const buttons = [];
@@ -39,12 +45,28 @@ for (let i = 0; i < 10; i++) {
   buttons[i].id = i;
   buttons[i].textContent = i;
   buttons[i].classList.add("numbers");
+  buttons[i].addEventListener("click", (e) => {
+    calcScreen.textContent = calcLogic(e.target.textContent);
+    console.log(firstNumber);
+  });
   calculator.appendChild(buttons[i]);
-};
+}
 
 const addButton = document.createElement("button");
 addButton.classList.add("operator");
 addButton.textContent = "+";
+addButton.addEventListener("click", () => {
+  if (itsFirst) {
+    itsFirst = false;
+    if (result) firstNumber = result;
+  } else {
+    result = operate(+firstNumber, +secondNumber, operator);
+    calcScreen.textContent = result;
+    firstNumber = result;
+    secondNumber = "";
+  }
+  operator = add;
+});
 calculator.appendChild(addButton);
 
 const subtractButton = document.createElement("button");
@@ -65,9 +87,25 @@ calculator.appendChild(divideButton);
 const operateButton = document.createElement("button");
 operateButton.classList.add("operator");
 operateButton.textContent = "=";
+operateButton.addEventListener("click", () => {
+  if (secondNumber) {
+    itsFirst = true;
+    result = operate(+firstNumber, +secondNumber, operator);
+    calcScreen.textContent = result;
+    firstNumber = "";
+    secondNumber = "";
+    operator = 0;
+  }
+});
 calculator.appendChild(operateButton);
 
 const clearButton = document.createElement("button");
 clearButton.classList.add("clear");
 clearButton.textContent = "CLEAR";
+clearButton.addEventListener("click", () => {
+  firstNumber = "";
+  secondNumber = "";
+  calcScreen.textContent = 0;
+  operator = 0;
+});
 calculator.appendChild(clearButton);
